0ac1dc16117cdb050320ae7effa5ed5394e5513c
Ajay Merchia <ajaymerchia@berkeley.edu>
2019-10-22 01:20:40 -0700

    fixed hex color initializer

--- ARMSwiftSuite/Classes/UISuite/AttributeExtensions.swift
+++ ARMSwiftSuite/Classes/UISuite/AttributeExtensions.swift
@@ -3,13 +3,13 @@ import SwiftUI
 @available(iOS 13.0, *)
 public extension Color {
 	init(hex: String) {
-		guard let rgb = Int(hex.replacingOccurrences(of: "#", with: "")) else {
+		guard let rgb = Int(hex.replacingOccurrences(of: "#", with: ""), radix: 16) else {
 			fatalError("Invalid hex string \"\(hex)\"")
 		}
 		self.init(
-			red: Double((rgb >> 16) & 0xFF),
-			green: Double((rgb >> 8) & 0xFF),
-			blue: Double(rgb & 0xFF)
+			red: Double((rgb >> 16) & 0xFF)/255,
+			green: Double((rgb >> 8) & 0xFF)/255,
+			blue: Double(rgb & 0xFF)/255
 		)
 	}
 }
